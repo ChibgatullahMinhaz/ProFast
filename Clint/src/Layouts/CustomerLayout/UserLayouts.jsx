f210efa2d0d35d82e0e3169d4849ca4645ae7fb1
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-07-29 00:09:26 +0600

    added dashboard layouts.

---
+++ Clint/src/Layouts/CustomerLayout/UserLayouts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserLayouts = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UserLayouts;
--- Clint/src/routes/routes.jsx
+++ Clint/src/routes/routes.jsx
@@ -4,6 +4,10 @@ import Landing from "../Pages/landingPage/Landing";
 import Home from "../Pages/Home/Home";
 import About from "../Pages/About/About";
 import Coverage from "../Pages/Coverege/Coverege";
+import AuthLayout from "../Layouts/AuthLayout/AuthLayout";
+import RiderLayout from "../Layouts/RiderLayout/RiderLayout";
+import UserLayouts from "../Layouts/CustomerLayout/UserLayouts";
+import AdminLayout from "../Layouts/AdminLayout/AdminLayout";
 
 export const router = createBrowserRouter([
   {
@@ -36,4 +40,44 @@ export const router = createBrowserRouter([
       },
     ],
   },
+
+  {
+    path: "/admin/dashboard",
+    element: <AdminLayout></AdminLayout>,
+  },
+  {
+    path: "/user/dashboard",
+    element: <UserLayouts></UserLayouts>,
+  },
+  {
+    path: "/rider/dashboard",
+    element: <RiderLayout />,
+  },
+  {
+    path: "auth",
+    element: <AuthLayout></AuthLayout>,
+    // children: [
+    //   {
+    //     path: "login",
+    //     Component: Login,
+    //   },
+    //   {
+    //     path: "register",
+    //     Component: Register,
+    //   },
+    // ],
+  },
+  // {
+  //   path: "/unauthorized",
+  //   element: (
+  //     <PrivateRoute>
+  //       <Unauthorized />,
+  //     </PrivateRoute>
+  //   ),
+  // },
+
+  // {
+  //   path: "*",
+  //   Component: NotFound,
+  // },
 ]);
